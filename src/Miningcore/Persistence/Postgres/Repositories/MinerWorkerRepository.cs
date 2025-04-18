ee7e808cb0cd46e72535d3f406c78d3fd083dd11
RetroMike <michaeldlesk@gmail.com>
2025-04-18 10:46:30 -0500

    Fixed PSQL Schemas for Worker Stats

--- src/Miningcore/Persistence/Postgres/Repositories/MinerWorkerRepository.cs
+++ src/Miningcore/Persistence/Postgres/Repositories/MinerWorkerRepository.cs
@@ -15,22 +15,22 @@ public class MinerWorkerRepository : IMinerWorkerRepository
 
     private readonly IMapper mapper;
 
-    public async Task<MinerWorkerStats> GetWorkerStatsAsync(IDbConnection con, IDbTransaction tx, string poolId, string address, string worker)
+    public async Task<MinerWorkerStats> GetWorkerStatsAsync(IDbConnection con, IDbTransaction tx, string poolId, string miner, string worker)
     {
-        const string query = @"SELECT * FROM workerstats WHERE poolid = @poolId AND address = @address AND worker = @worker";
+        const string query = @"SELECT * FROM workerstats WHERE poolid = @poolId AND miner = @miner AND worker = @worker";
 
-        var entity = await con.QuerySingleOrDefaultAsync<Entities.MinerWorkerStats>(query, new {poolId, address, worker}, tx);
+        var entity = await con.QuerySingleOrDefaultAsync<Entities.MinerWorkerStats>(query, new {poolId, miner, worker}, tx);
 
         return mapper.Map<MinerWorkerStats>(entity);
     }
 
     public Task UpdateWorkerStatsAsync(IDbConnection con, IDbTransaction tx, MinerWorkerStats settings)
     {
-        const string query = @"INSERT INTO workerstats(poolid, address, worker, bestdifficulty, created, updated)
-            VALUES(@poolid, @address, @worker, @bestdifficulty, now(), now())
+        const string query = @"INSERT INTO workerstats(poolid, miner, worker, bestdifficulty, created, updated)
+            VALUES(@poolid, @miner, @worker, @bestdifficulty, now(), now())
             ON CONFLICT ON CONSTRAINT workerstats_pkey DO UPDATE
             SET bestdifficulty = @bestdifficulty, updated = now()
-            WHERE workerstats.poolid = @poolid AND workerstats.address = @address AND workerstats.worker = @worker";
+            WHERE workerstats.poolid = @poolid AND workerstats.miner = @miner AND workerstats.worker = @worker";
 
         return con.ExecuteAsync(query, settings, tx);
     }
--- src/Miningcore/Persistence/Postgres/Scripts/createdb.sql
+++ src/Miningcore/Persistence/Postgres/Scripts/createdb.sql
@@ -34,6 +34,8 @@ CREATE TABLE blocks
 	reward decimal(28,12) NULL,
     source TEXT NULL,
     hash TEXT NULL,
+    worker TEXT NULL,
+    difficulty DOUBLE PRECISION NULL,
 	created TIMESTAMPTZ NOT NULL
 );
 
@@ -123,16 +125,17 @@ CREATE INDEX IDX_MINERSTATS_POOL_MINER_WORKER_CREATED_HASHRATE on minerstats(poo
 
 CREATE TABLE workerstats
 (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	poolid TEXT NOT NULL,
+    poolid TEXT NOT NULL,
 	miner TEXT NOT NULL,
-	worker TEXT NOT NULL,
-  bestdifficulty DOUBLE PRECISION NOT NULL DEFAULT 0,
-	created TIMESTAMPTZ NOT NULL
+    worker TEXT NOT NULL,
+	bestdifficulty DOUBLE PRECISION NOT NULL DEFAULT 0,
+	created TIMESTAMPTZ NOT NULL,
+	updated TIMESTAMPTZ NOT NULL,
+
+	primary key(poolid, miner, worker)
 );
 
 CREATE INDEX IDX_WORKERSTATS_POOL_CREATED on workerstats(poolid, created);
 CREATE INDEX IDX_WORKERSTATS_POOL_MINER_CREATED on workerstats(poolid, miner, created);
+CREATE INDEX IDX_WORKERSTATS_POOL_MINER__WORKER_CREATED on workerstats(poolid, miner, worker, created);
 CREATE INDEX IDX_WORKERSTATS_POOL_MINER_WORKER_CREATED_BESTDIFFICULTY on workerstats(poolid,miner,worker,created desc,bestdifficulty);
-
-ALTER TABLE blocks ADD COLUMN IF NOT EXISTS worker TEXT NULL;
