58e3b08a3c041b7f30dbcb0c46e5288934581c7c
RetroMike <michaeldlesk@gmail.com>
2025-04-17 23:04:08 -0500

    Update Share.cs

--- src/Miningcore/Blockchain/Share.cs
+++ src/Miningcore/Blockchain/Share.cs
@@ -101,4 +101,10 @@ public class Share
     /// </summary>
     [ProtoMember(15)]
     public DateTime Created { get; set; }
+
+    /// <summary>
+    /// Difficulty of the share that was submitted (used for reporting on best difficulty and track difficulty use to solve block)
+    /// </summary>
+    [ProtoMember(16)]
+    public double ShareDifficulty { get; set; }
 }
