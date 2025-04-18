3fbffd9df014f1c3f5b33b5e01f531a512df961c
RetroMike <michaeldlesk@gmail.com>
2025-04-17 22:09:35 -0500

    Update Block.cs

--- src/Miningcore/Persistence/Postgres/Entities/Block.cs
+++ src/Miningcore/Persistence/Postgres/Entities/Block.cs
@@ -16,5 +16,6 @@ public class Block
     public decimal Reward { get; set; }
     public string Source { get; set; }
     public string Hash { get; set; }
+    public string Worker { get; set; }
     public DateTime Created { get; set; }
 }
