5b5c97b493377e9e4ebdaa6322d156b7aeb547a5
RetroMike <michaeldlesk@gmail.com>
2025-04-28 22:56:59 -0500

    fix

--- src/Miningcore/Persistence/Postgres/Repositories/PaymentRepository.cs
+++ src/Miningcore/Persistence/Postgres/Repositories/PaymentRepository.cs
@@ -74,7 +74,7 @@ public class PaymentRepository : IPaymentRepository
     public async Task<BalanceChange[]> PageBalanceChangesAsync(IDbConnection con, string poolId, string address, int page, int pageSize, string usageFilter, CancellationToken ct)
     {
        string query = @"SELECT * FROM balance_changes WHERE poolid = @poolid
-            AND address = @address AND usage like '%@usageFilter%'
+            AND address = @address AND usage like '% @usageFilter %'
             ORDER BY created DESC OFFSET @offset FETCH NEXT @pageSize ROWS ONLY";
 
         return (await con.QueryAsync<Entities.BalanceChange>(new CommandDefinition(query,
