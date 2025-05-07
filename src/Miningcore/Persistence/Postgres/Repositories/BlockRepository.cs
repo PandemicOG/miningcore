d7fb5b331faf13d3c30428d00629681952dd24da
RetroMike <michaeldlesk@gmail.com>
2025-05-07 18:52:33 -0500

    Limit processing of pending blocks per batch

--- src/Miningcore/Persistence/Postgres/Repositories/BlockRepository.cs
+++ src/Miningcore/Persistence/Postgres/Repositories/BlockRepository.cs
@@ -96,7 +96,7 @@ public class BlockRepository : IBlockRepository
 
     public async Task<Block[]> GetPendingBlocksForPoolAsync(IDbConnection con, string poolId)
     {
-        const string query = @"SELECT * FROM blocks WHERE poolid = @poolid AND status = @status";
+        const string query = @"SELECT * FROM blocks WHERE poolid = @poolid AND status = @status LIMIT 500";
 
         return (await con.QueryAsync<Entities.Block>(query, new { status = BlockStatus.Pending.ToString().ToLower(), poolid = poolId }))
             .Select(mapper.Map<Block>)
